3af8fa498d4ab01d732accbdb22f3837b9a3ff84
headlessNode <harriskhan047@outlook.com>
2023-12-12 15:33:04 +0500

    add dom.js to create page elements

---
+++ src/script/dom.js
@@ -0,0 +1,36 @@
+import Image from '../images/mountain.png';
+
+export function createInitialPage() {
+  const wrapper = document.querySelector('.wrapper');
+
+  const searchContainer = document.createElement('div');
+  searchContainer.setAttribute('class', 'search-container');
+
+  const userSearch = document.createElement('input');
+  userSearch.setAttribute('type', 'text');
+  userSearch.setAttribute('name', 'search-city');
+  userSearch.setAttribute('id', 'search-city');
+  userSearch.setAttribute('class', 'search-city');
+  userSearch.setAttribute('placeholder', 'Enter city name');
+  const searchBtn = document.createElement('button');
+  searchBtn.setAttribute('class', 'search-btn');
+  searchBtn.innerHTML = '<svg class="search-icon" width="100%" height="100%" viewBox="-3.2 -3.2 38.4 38.4" xmlns="http://www.w3.org/2000/svg"><path d="M13.46 24.45c-6.29 0-11.389-5.01-11.389-11.2 0-6.19 5.099-11.21 11.389-11.21 6.29 0 11.39 5.02 11.39 11.21 0 6.19-5.1 11.2-11.39 11.2Zm18.228 5.8-8.259-8.13c2.162-2.35 3.491-5.45 3.491-8.87C26.92 5.93 20.894 0 13.46 0 6.026 0 0 5.93 0 13.25c0 7.31 6.026 13.24 13.46 13.24a13.52 13.52 0 0 0 8.472-2.96l8.292 8.16c.405.4 1.06.4 1.464 0 .405-.39.405-1.04 0-1.44Z" fill="#000" fill-rule="evenodd"/></svg>';
+
+  searchContainer.appendChild(userSearch);
+  searchContainer.appendChild(searchBtn);
+
+  wrapper.appendChild(searchContainer);
+}
+
+export function createWeatherCard(weatherInfo) {
+  const wrapper = document.querySelector('.wrapper');
+  const weatherCard = document.createElement('div');
+  weatherCard.setAttribute('class', 'weather-card');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', Image);
+  img.setAttribute('class', 'mountain');
+
+  weatherCard.appendChild(img);
+  wrapper.appendChild(weatherCard);
+}
