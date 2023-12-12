c830f6534732adda8ece768e1bc063fc017bb37e
headlessNode <harriskhan047@outlook.com>
2023-12-12 12:08:52 +0500

    get location from user and console log the weather data

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,12 @@
+import getWeatherData from './getWeatherInfo';
 import '../style/style.css';
+
+const userSearch = document.querySelector('.search-city');
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', () => {
+  const location = userSearch.value;
+  getWeatherData(location).then((response) => {
+    console.log(response);
+  });
+});
