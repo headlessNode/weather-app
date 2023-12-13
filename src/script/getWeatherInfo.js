42c29f491744c7bbe64e9d3debca74355e842a2f
headlessNode <harriskhan047@outlook.com>
2023-12-13 23:24:26 +0500

    Add logic for getting weather forecast - add logic for getting date - modify the weatherInfo object to get forecast of next two days

--- src/script/getWeatherInfo.js
+++ src/script/getWeatherInfo.js
@@ -3,7 +3,7 @@
 // and return the weather data for that location. For now, just console.log() the information.
 
 function getWeatherData(location) {
-  return fetch(`https://api.weatherapi.com/v1/current.json?key=a1cd623f96c04f989fa83523230812&q=${location}`, { mode: 'cors' })
+  return fetch(`https://api.weatherapi.com/v1/forecast.json?key=a1cd623f96c04f989fa83523230812&q=${location}&days=3`, { mode: 'cors' })
     .then((res) => {
       if (!res.ok) {
         throw new Error(`${res.status} ${res.statusText}`);
@@ -12,16 +12,50 @@ function getWeatherData(location) {
       }
     })
     .then((weatherData) => {
+      const date = new Date(weatherData.location.localtime_epoch * 1000);
+      const year = date.getFullYear() - 2000;
+      const month = date.getMonth();
+      const dayDate = date.getDate();
+      const day = date.getDay();
+      const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+      const tomorrowForecastDate = new Date(weatherData.forecast.forecastday[1].date_epoch * 1000);
+      const tomorrowDate = `${tomorrowForecastDate.getDate()} ${months[tomorrowForecastDate.getMonth()]} ${year}`;
+      const tomorrowDay = `${weekday[tomorrowForecastDate.getDay()]}`;
+
+      const dayAfterTomorrow = new Date(weatherData.forecast.forecastday[2].date_epoch * 1000);
+      const dayAfterTomorrowDate = `${dayAfterTomorrow.getDate()} ${months[dayAfterTomorrow.getMonth()]} ${year}`;
+      const afterTomorrowDay = `${weekday[dayAfterTomorrow.getDay()]}`;
+
       const weatherInfo = {
-        currentWeather: weatherData.current.condition.text,
-        temperature: {
-          tempC: weatherData.current.temp_c,
-          tempF: weatherData.current.temp_f,
-          humidity: weatherData.current.humidity,
+        currentWeather: {
+          condition: weatherData.current.condition.text,
+          temperature: {
+            tempC: weatherData.current.temp_c,
+            tempF: weatherData.current.temp_f,
+            humidity: weatherData.current.humidity,
+          },
+        },
+        forecast: {
+          tomorrow: {
+            condition: weatherData.forecast.forecastday[1].day.condition,
+            maxTemp: weatherData.forecast.forecastday[1].day.maxtemp_c,
+            date: tomorrowDate,
+            day: tomorrowDay,
+          },
+          dayAfterTomorrow: {
+            condition: weatherData.forecast.forecastday[2].day.condition,
+            maxTemp: weatherData.forecast.forecastday[2].day.maxtemp_c,
+            date: dayAfterTomorrowDate,
+            day: afterTomorrowDay,
+          },
         },
         region: {
           city: weatherData.location.name,
           country: weatherData.location.country,
+          localDate: `${dayDate} ${months[month]} ${year}`,
+          localDay: `${weekday[day]}`,
         },
       };
 
