ec3e65bccb84f0f55dcc2de3e16c464310d5f286
headlessNode <harriskhan047@outlook.com>
2023-12-15 01:13:57 +0500

    add logic for creating muliple page elements - In index.js add logic for creating a weathercard for a default   location. - add logic for, adding feelslike temperature, humidity, windspeed, and   temperature in fahrenheit to the weathercard. - add logic for changing background and icon based on daytime or night.

--- src/script/dom.js
+++ src/script/dom.js
@@ -1,49 +1,89 @@
 import Image from '../images/sky.png';
-
-export function createInitialPage() {
-  const wrapper = document.querySelector('.wrapper');
-
-  const imageContainer = document.createElement('div');
-  imageContainer.setAttribute('class', 'img-container');
-
-  const img = document.createElement('img');
-  img.setAttribute('src', Image);
-  img.setAttribute('class', 'mountain');
-
-  imageContainer.appendChild(img);
-
-  const weatherCardContainer = document.createElement('div');
-  weatherCardContainer.setAttribute('class', 'card-container');
-
-  const searchContainer = document.createElement('div');
-  searchContainer.setAttribute('class', 'search-container');
-
-  const userSearch = document.createElement('input');
-  userSearch.setAttribute('type', 'text');
-  userSearch.setAttribute('name', 'search-city');
-  userSearch.setAttribute('id', 'search-city');
-  userSearch.setAttribute('class', 'search-city');
-  userSearch.setAttribute('placeholder', 'Enter city name');
-  const searchBtn = document.createElement('button');
-  searchBtn.setAttribute('class', 'search-btn');
-  searchBtn.innerHTML = '<svg class="search-icon" width="100%" height="100%" viewBox="-3.2 -3.2 38.4 38.4" xmlns="http://www.w3.org/2000/svg"><path d="M13.46 24.45c-6.29 0-11.389-5.01-11.389-11.2 0-6.19 5.099-11.21 11.389-11.21 6.29 0 11.39 5.02 11.39 11.21 0 6.19-5.1 11.2-11.39 11.2Zm18.228 5.8-8.259-8.13c2.162-2.35 3.491-5.45 3.491-8.87C26.92 5.93 20.894 0 13.46 0 6.026 0 0 5.93 0 13.25c0 7.31 6.026 13.24 13.46 13.24a13.52 13.52 0 0 0 8.472-2.96l8.292 8.16c.405.4 1.06.4 1.464 0 .405-.39.405-1.04 0-1.44Z" fill="#000" fill-rule="evenodd"/></svg>';
-
-  searchContainer.appendChild(userSearch);
-  searchContainer.appendChild(searchBtn);
-  weatherCardContainer.appendChild(searchContainer);
-
-  wrapper.appendChild(imageContainer);
-  wrapper.appendChild(weatherCardContainer);
-}
+import HumidityIcon from '../images/humidityIcon.svg';
+import WindIcon from '../images/windIcon.svg';
+import FahrenheitIcon from '../images/fahrenheit.svg';
 
 function addCurrentWeatherDetails(weatherInfo) {
-  const temperature = document.createElement('div');
+  const temperature = document.createElement('p');
   temperature.setAttribute('class', 'temperature');
   const degreeSign = '\u00B0';
   temperature.textContent = `${weatherInfo.currentWeather.temperature.tempC}${degreeSign}C`;
+
+  const icon = document.createElement('div');
+  icon.setAttribute('class', 'icon');
+  if (weatherInfo.region.isDay === 'yes') {
+    icon.style.background = 'radial-gradient(circle at 30% 60%, #ffd700 0%, transparent 70%)';
+  } else {
+    icon.style.background = 'radial-gradient(circle at 30% 60%, rgb(204,204,204) 0%, transparent 70%)';
+  }
   const weather = document.createElement('div');
   weather.setAttribute('class', 'weather');
   weather.textContent = weatherInfo.currentWeather.condition;
+  const crntWeatherDetails = document.createElement('div');
+  crntWeatherDetails.setAttribute('class', 'crnt-weather-details');
+
+  const humidity = document.createElement('div');
+  humidity.setAttribute('class', 'humidity');
+  const humidityIcon = document.createElement('img');
+  humidityIcon.setAttribute('class', 'humidity-icon');
+  humidityIcon.src = HumidityIcon;
+  const humidityHeading = document.createElement('div');
+  humidityHeading.textContent = 'Humidity';
+  const humidityPercent = document.createElement('div');
+  humidityPercent.textContent = `${weatherInfo.currentWeather.temperature.humidity}%`;
+
+  humidity.appendChild(humidityIcon);
+  humidity.appendChild(humidityHeading);
+  humidity.appendChild(humidityPercent);
+
+  const feelsLike = document.createElement('div');
+  feelsLike.setAttribute('class', 'feels-like');
+  const feelsLikeIcon = document.createElement('div');
+  feelsLikeIcon.setAttribute('class', 'feels-like-icon');
+  feelsLikeIcon.innerHTML = '<!doctype html><svg width="40" height="40" viewBox="0 0 40 40"><path style="fill:rgb(255,255,255);" d="M 30 34 L 26 34 C 24.897 33.997 24.004 33.103 24 32 L 24 25 C 22.897 24.997 22.004 24.103 22 23 L 22 17 C 21.969 15.33 23.331 13.969 25 14 L 31 14 C 32.67 13.969 34.031 15.33 34 17 L 34 23 C 33.997 24.103 33.104 24.997 32 25 L 32 32 C 31.997 33.103 31.104 33.997 30 34 Z M 25 16 C 24.435 15.966 23.966 16.434 24 17 L 24 23 L 26 23 L 26 32 L 30 32 L 30 23 L 32 23 L 32 17 C 32.034 16.434 31.566 15.966 31 16 L 25 16 Z M 28 13 C 24.921 13 22.997 9.667 24.536 7 C 26.076 4.333 29.925 4.333 31.464 7 C 31.815 7.608 32 8.298 32 9 C 31.994 11.206 30.207 12.993 28 13 Z M 28 7 C 26.461 7 25.498 8.667 26.268 10 C 27.038 11.333 28.963 11.333 29.732 10 C 29.908 9.696 30 9.351 30 9 C 29.997 7.897 29.104 7.003 28 7 Z M 14 24.184 L 14 16 L 12 16 L 12 24.184 C 9.823 24.954 9.295 27.792 11.051 29.293 C 12.806 30.793 15.528 29.831 15.95 27.561 C 16.22 26.109 15.393 24.676 14 24.184 Z" transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, 0)"></path><path style="fill:rgb(255,255,255);" d="M 13 34 C 7.617 34.006 4.246 28.182 6.932 23.517 C 7.227 23.005 7.586 22.532 8 22.11 L 8 11 C 8 7.151 12.167 4.745 15.5 6.67 C 17.047 7.563 18 9.214 18 11 L 18 22.11 C 21.77 25.953 19.966 32.436 14.753 33.779 C 14.18 33.926 13.592 34.001 13 34 Z M 13 8 C 11.344 8.002 10.002 9.344 10 11 L 10 22.983 L 9.668 23.282 C 6.799 25.847 7.782 30.557 11.438 31.76 C 15.094 32.962 18.681 29.756 17.895 25.988 C 17.677 24.942 17.129 23.994 16.332 23.282 L 16 22.983 L 16 11 C 15.999 9.344 14.656 8.002 13 8 Z" transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, 0)"></path><path data-name="<Transparent Rectangle>" style="fill:none" d="M 4 4 L 36 4 L 36 36 L 4 36 L 4 4 Z" transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, 0)"></path></svg>';
+  const feelsLikeHeading = document.createElement('div');
+  feelsLikeHeading.textContent = 'Feels like';
+  const feelsLikeTemp = document.createElement('div');
+  feelsLikeTemp.textContent = `${weatherInfo.currentWeather.temperature.feelsLike}${degreeSign}C`;
+
+  feelsLike.appendChild(feelsLikeIcon);
+  feelsLike.appendChild(feelsLikeHeading);
+  feelsLike.appendChild(feelsLikeTemp);
+
+  const windSpeed = document.createElement('div');
+  windSpeed.setAttribute('class', 'wind-speed');
+  const windIcon = document.createElement('img');
+  windIcon.setAttribute('class', 'wind-icon');
+  windIcon.src = WindIcon;
+  const windHeading = document.createElement('div');
+  windHeading.textContent = 'Wind speed';
+  const windKph = document.createElement('div');
+  windKph.textContent = `${weatherInfo.currentWeather.temperature.windKph}KPH`;
+
+  windSpeed.appendChild(windIcon);
+  windSpeed.appendChild(windHeading);
+  windSpeed.appendChild(windKph);
+
+  const tempF = document.createElement('div');
+  tempF.setAttribute('class', 'temp-f');
+  const fahrenheitIcon = document.createElement('img');
+  fahrenheitIcon.setAttribute('class', 'fahrenheit-icon');
+  fahrenheitIcon.src = FahrenheitIcon;
+  const fahrenheitHeading = document.createElement('div');
+  fahrenheitHeading.textContent = 'Fahrenheit';
+  const fahrenheitTemp = document.createElement('div');
+  fahrenheitTemp.textContent = `${weatherInfo.currentWeather.temperature.tempF}${degreeSign}F`;
+
+  tempF.appendChild(fahrenheitIcon);
+  tempF.appendChild(fahrenheitHeading);
+  tempF.appendChild(fahrenheitTemp);
+
+  crntWeatherDetails.appendChild(humidity);
+  crntWeatherDetails.appendChild(feelsLike);
+  crntWeatherDetails.appendChild(windSpeed);
+  crntWeatherDetails.appendChild(tempF);
+
+  weather.appendChild(crntWeatherDetails);
 
   const region = document.createElement('div');
   region.setAttribute('class', 'region');
@@ -54,7 +94,7 @@ function addCurrentWeatherDetails(weatherInfo) {
   currentDay.textContent = `${weatherInfo.region.localDay}, ${weatherInfo.region.localDate}`;
 
   return {
-    temperature, weather, region, currentDay,
+    temperature, weather, region, currentDay, icon,
   };
 }
 
@@ -112,10 +152,25 @@ function removeWeatherCard() {
   }
 }
 
+function changeSceneBackground(weatherInfo) {
+  const sceneContainer = document.querySelector('.scene-container');
+  if (weatherInfo.region.isDay === 'yes') {
+    if (sceneContainer.classList.contains('night')) {
+      sceneContainer.classList.remove('night');
+    }
+    sceneContainer.classList.add('day');
+  } else {
+    if (sceneContainer.classList.contains('day')) {
+      sceneContainer.classList.remove('day');
+    }
+    sceneContainer.classList.add('night');
+  }
+}
+
 export function createWeatherCard(weatherInfo) {
   console.log(weatherInfo);
   removeWeatherCard();
-
+  changeSceneBackground(weatherInfo);
   const weatherCardContainer = document.querySelector('.card-container');
   weatherCardContainer.classList.toggle('card-present');
   const weatherCard = document.createElement('div');
@@ -123,8 +178,6 @@ export function createWeatherCard(weatherInfo) {
 
   const currentWeatherDetails = addCurrentWeatherDetails(weatherInfo);
 
-  // const weatherIcon = document.createElement('div');
-  // weatherIcon.setAttribute('class', 'weather-icon');
   const forecastDetails = document.createElement('div');
   forecastDetails.setAttribute('class', 'forecast-details');
   const forecast = addForecastDetails(weatherInfo);
@@ -133,10 +186,47 @@ export function createWeatherCard(weatherInfo) {
   forecastDetails.appendChild(forecast.dayAfterTomorrow);
 
   weatherCard.appendChild(currentWeatherDetails.temperature);
+  weatherCard.appendChild(currentWeatherDetails.icon);
   weatherCard.appendChild(currentWeatherDetails.weather);
   weatherCard.appendChild(currentWeatherDetails.region);
   weatherCard.appendChild(currentWeatherDetails.currentDay);
   weatherCard.appendChild(forecastDetails);
 
   weatherCardContainer.appendChild(weatherCard);
+}
+
+export function createInitialPage(weatherInfo) {
+  // const wrapper = document.querySelector('.wrapper');
+
+  const sceneContainer = document.createElement('div');
+  sceneContainer.setAttribute('class', 'scene-container');
+  document.body.appendChild(sceneContainer);
+  changeSceneBackground(weatherInfo);
+  const img = document.createElement('img');
+  img.setAttribute('src', Image);
+  img.setAttribute('class', 'scene');
+
+  sceneContainer.appendChild(img);
+
+  const weatherCardContainer = document.createElement('div');
+  weatherCardContainer.setAttribute('class', 'card-container');
+  sceneContainer.appendChild(weatherCardContainer);
+
+  const searchContainer = document.createElement('div');
+  searchContainer.setAttribute('class', 'search-container');
+
+  const userSearch = document.createElement('input');
+  userSearch.setAttribute('type', 'text');
+  userSearch.setAttribute('name', 'search-city');
+  userSearch.setAttribute('id', 'search-city');
+  userSearch.setAttribute('class', 'search-city');
+  userSearch.setAttribute('placeholder', 'Enter city name');
+  const searchBtn = document.createElement('button');
+  searchBtn.setAttribute('class', 'search-btn');
+  searchBtn.innerHTML = '<svg class="search-icon" width="100%" height="100%" viewBox="-3.2 -3.2 38.4 38.4" xmlns="http://www.w3.org/2000/svg"><path d="M13.46 24.45c-6.29 0-11.389-5.01-11.389-11.2 0-6.19 5.099-11.21 11.389-11.21 6.29 0 11.39 5.02 11.39 11.21 0 6.19-5.1 11.2-11.39 11.2Zm18.228 5.8-8.259-8.13c2.162-2.35 3.491-5.45 3.491-8.87C26.92 5.93 20.894 0 13.46 0 6.026 0 0 5.93 0 13.25c0 7.31 6.026 13.24 13.46 13.24a13.52 13.52 0 0 0 8.472-2.96l8.292 8.16c.405.4 1.06.4 1.464 0 .405-.39.405-1.04 0-1.44Z" fill="#000" fill-rule="evenodd"/></svg>';
+
+  searchContainer.appendChild(userSearch);
+  searchContainer.appendChild(searchBtn);
+  weatherCardContainer.appendChild(searchContainer);
+  createWeatherCard(weatherInfo);
 }
--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,12 @@
 import '../style/style.css';
 import searchFormSubmit from './eventHandling';
 import { createInitialPage } from './dom';
+import getWeatherData from './getWeatherInfo';
 
-createInitialPage();
-
-const events = (() => {
+const defaultLocation = 'London';
+getWeatherData(defaultLocation).then((weatherInfo) => {
+  createInitialPage(weatherInfo);
+}).then(() => {
   const submitBtn = document.querySelector('.search-btn');
   submitBtn.addEventListener('click', searchFormSubmit);
-})();
+});
