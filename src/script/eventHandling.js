06aa9fe903326483485fca34dda01ae006ca88b5
headlessNode <harriskhan047@outlook.com>
2023-12-16 22:41:46 +0500

    Call functions related to loading animation

--- src/script/eventHandling.js
+++ src/script/eventHandling.js
@@ -1,10 +1,15 @@
 import getWeatherData from './getWeatherInfo';
-import { createWeatherCard, invalidLocation } from './dom';
+import {
+  createWeatherCard, invalidLocation, removeWeatherCard, addLoadingAnim, removeLoadingAnim,
+} from './dom';
 
 function searchFormSubmit() {
   const userSearch = document.querySelector('.search-city');
   const location = userSearch.value;
+  removeWeatherCard();
+  addLoadingAnim();
   getWeatherData(location).then((response) => {
+    removeLoadingAnim();
     createWeatherCard(response);
   }, (err) => {
     if (err.message.includes('400')) {
