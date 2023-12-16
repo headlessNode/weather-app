f5d919a56e03c3299b60939e99a6de39e160988e
headlessNode <harriskhan047@outlook.com>
2023-12-16 22:40:46 +0500

    Call function for initial page loading animation and removing the animation

--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,12 @@
 import '../style/style.css';
 import searchFormSubmit from './eventHandling';
-import { createInitialPage } from './dom';
+import { createInitialPage, initialPageLoader, removeInitialPageLoader } from './dom';
 import getWeatherData from './getWeatherInfo';
 
 const defaultLocation = 'London';
+initialPageLoader();
 getWeatherData(defaultLocation).then((weatherInfo) => {
+  removeInitialPageLoader();
   createInitialPage(weatherInfo);
 }).then(() => {
   const searchForm = document.querySelector('.search-container');
